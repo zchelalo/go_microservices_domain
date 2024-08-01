386a1b98007df6037c087934ba498da49e54ebef
zchelalo <eduardosaavedra687@gmail.com>
2024-08-01 12:59:12 -0700

    add: type EnrollmentStatus for Status

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -8,16 +8,25 @@ import (
 )
 
 type Enrollment struct {
-	Id        string         `json:"id" gorm:"type:char(36);not null;primary_key"`
-	UserId    string         `json:"user_id,omitempty" gorm:"type:char(36);not null"`
-	User      *User          `json:"user,omitempty" gorm:"-"`
-	CourseId  string         `json:"course_id,omitempty" gorm:"type:char(36);not null"`
-	Course    *Course        `json:"course,omitempty" gorm:"-"`
-	Status    string         `json:"status" gorm:"type:char(2)"`
-	CreatedAt *time.Time     `json:"-"`
-	UpdatedAt *time.Time     `json:"-"`
-	DeletedAt gorm.DeletedAt `json:"-"`
+	Id        string           `json:"id" gorm:"type:char(36);not null;primary_key"`
+	UserId    string           `json:"user_id,omitempty" gorm:"type:char(36);not null"`
+	User      *User            `json:"user,omitempty" gorm:"-"`
+	CourseId  string           `json:"course_id,omitempty" gorm:"type:char(36);not null"`
+	Course    *Course          `json:"course,omitempty" gorm:"-"`
+	Status    EnrollmentStatus `json:"status" gorm:"type:char(2)"`
+	CreatedAt *time.Time       `json:"-"`
+	UpdatedAt *time.Time       `json:"-"`
+	DeletedAt gorm.DeletedAt   `json:"-"`
 }
+
+type EnrollmentStatus string
+
+const (
+	Pending  EnrollmentStatus = "PP"
+	Active   EnrollmentStatus = "AA"
+	Studying EnrollmentStatus = "SS"
+	Inactive EnrollmentStatus = "II"
+)
 
 func (enrollment *Enrollment) BeforeCreate(tx *gorm.DB) (err error) {
 	if enrollment.Id == "" {
